b5caf98f4316f866c3a51a0e0ef4b1e3fa9e19f1
moonshine <710159940@qq.com>
2020-12-03 23:13:07 +0800

    操作日志:     (1)添加音乐文件

---
+++ src/components/music.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="music">
+
+   </div>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue'
+    export default defineComponent({
+        name: 'music',
+
+    })
+</script>
+
+<style scoped>
+
+</style>
